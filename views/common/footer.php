6cae45e289e5185b73890a543231475b4727d9aa
Mehjabin Mostafa <malihamehjabin.mostafa08@gmail.com>
2026-01-22 12:39:44 +0600

    Load scripts conditionally based on user role

--- views/common/footer.php
+++ views/common/footer.php
@@ -3,6 +3,12 @@
 
 
 <script src="js/auth.js"></script>
-<!-- Load other scripts dynamically based on page if needed -->
+<?php if(isset($_SESSION['user_role'])): ?>
+    <?php if($_SESSION['user_role'] === 'director'): ?>
+        <script src="js/admin.js"></script>
+    <?php elseif($_SESSION['user_role'] === 'astronaut'): ?>
+        <script src="js/astronaut.js"></script>
+    <?php endif; ?>
+<?php endif; ?>
 </body>
 </html>
